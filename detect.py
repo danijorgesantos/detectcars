5f8e05db55edefec694a1e437f3dec06118e7cb0
Daniel Santos <daniel.santos@rupealgroup.onmicrosoft.com>
2020-07-11 00:17:47 +0100

    asd

--- detect.py
+++ detect.py
@@ -2,23 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# # read the image
-# image = cv2.imread("car.jpg")
+# read the image
+image = cv2.imread("car.jpg")
 
-# # convert it to grayscale
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# convert it to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# # perform the canny edge detector to detect image edges
-# edges = cv2.Canny(gray, threshold1=30, threshold2=100)
+# perform the canny edge detector to detect image edges
+edges = cv2.Canny(gray, threshold1=30, threshold2=100)
 
-# # detect lines in the image using hough lines technique
-# lines = cv2.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 50, 5)
+# detect lines in the image using hough lines technique
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 60, np.array([]), 50, 5)
 
-# # iterate over the output lines and draw them
-# for line in lines:
-#     for x1, y1, x2, y2 in line:
-#         cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
+# iterate over the output lines and draw them
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
 
-# # show the grayscale image
-# plt.imshow(image, cmap="gray")
-# plt.show()
+# show the grayscale image
+plt.imshow(image, cmap="gray")
+plt.show()
